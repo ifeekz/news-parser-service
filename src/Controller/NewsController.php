11bc5c06efb6764097240db56cbc26050cb61b1f
Ifeanyi Nnorom <nnoromifeanyi@gmail.com>
2022-09-28 04:52:12 +0100

    upd: created news parser service

--- src/Controller/NewsController.php
+++ src/Controller/NewsController.php
@@ -6,6 +6,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
 
+use App\Service\NewsParser;
+
 class NewsController extends AbstractController
 {
     /**
@@ -13,7 +15,14 @@ class NewsController extends AbstractController
      */
     public function index(): Response
     {
-        $news = "";
+        $data = [
+            'url' => 'https://highload.today/',
+            'titlesXPath' => '//div[@class="col sidebar-center"]//div[@class="lenta-item"]//a//h2',
+            'descriptionsXPath' => '//div[@class="col sidebar-center"]//div[@class="lenta-item"]//p',
+            'picturesXPath' => '//div[@class="col sidebar-center"]//div[@class="lenta-item"]//div[@class="lenta-image"]//img'
+        ];
+                
+        $news = NewsParser::scrape($data);
 
         return $this->render('news/index.html.twig', [
             'news' => $news,
--- src/Service/NewsParser.php
+++ src/Service/NewsParser.php
@@ -2,12 +2,45 @@
 
 namespace App\Service;
 
+use Goutte\Client;
+
 class NewsParser
 {
-    public function scrape(): string
+    /**
+     * @param data
+     */
+    public static function scrape($data): array
     {
-        // TO DO scraping logic
+        $url = $data['url'];
+        $titlesXPath = $data['titlesXPath'];
+        $descriptionsXPath = $data['descriptionsXPath'];
+        $picturesXPath = $data['picturesXPath'];
+        
+        $httpClient = new Client();
+        $response = $httpClient->request('GET', $url);
+        $titles = $response->evaluate($titlesXPath);
+        $descriptions = $response->evaluate($descriptionsXPath);
+        $pictures = $response->evaluate($picturesXPath)->extract(['src']);
+        
+        $newsArray = [];
+        $descriptionsArray = [];
+        $picturesArray = [];
+        foreach ($descriptions as $key => $description) {
+            $descriptionsArray[] = ['description' => $description->textContent];
+        }
 
-        return '';
+        foreach ($pictures as $key => $picture) {
+            $picturesArray[] = ['picture' => $picture];
+        }
+        
+        foreach ($titles as $key => $title) {
+            $newsArray[] = [
+                'title' => $title->textContent, 
+                'description' => $descriptionsArray[$key],
+                'picture' => $picturesArray[$key]
+            ];
+        }
+
+        return $newsArray;
     }
 }
